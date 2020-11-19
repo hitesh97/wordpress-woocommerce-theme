f0680e4914e3c21a67d24b4029c8ad90bf15b8bc
Khatri Hitesh <khatri.hitesh@gmail.com>
2020-11-19 12:28:38 +0000

    woo commerce templates now imported and customized

--- woocommerce-theme/functions.php
+++ woocommerce-theme/functions.php
@@ -46,3 +46,10 @@ register_sidebar(
         'after_title' => '</h4>'
     )
 );
+
+function mytheme_add_woocommerce_support()
+{
+    add_theme_support('woocommerce');
+}
+
+add_action('after_setup_theme', 'mytheme_add_woocommerce_support');
---
+++ woocommerce-theme/shop-page.php
@@ -0,0 +1,25 @@
+<?php
+/*
+Template Name: Shop Template
+*/
+
+get_header(); ?>
+
+<div class="container content">
+
+    <div class='row'>
+        <div class='col-lg-12'>
+            <h1><?php the_title(); ?></h1>
+            <?php if (have_posts()) : while (have_posts()) : the_post(); ?>
+                    <?php the_content(); ?>
+            <?php endwhile;
+            else : endif; ?>
+
+        </div>
+
+    </div>
+
+</div>
+
+
+<?php get_footer(); ?>
